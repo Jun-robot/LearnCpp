65365e41518c79e43ebf9d1a08696876c00afe23
Jun-robot <saijun721.1@gmail.com>
2024-11-22 15:22:43 +0900

    2進数変換

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -1,14 +1,48 @@
-
-int factorial( int n )
+// 1,0のみを使った10進数から
+// 2進数へ変換する関数
+int solve( int n )
 {
-    if ( n < 1 )
-        return 0 ;
-    else if ( n == 1 )
-        return 1 ;
+    if ( n <= 1 )
+        return n ;
     else
-        return n * factorial(n-1) ;
+        return n%10 + 2 * solve( n/10 ) ;
 }
 
-int main(){
-    std::cout << factorial(4);
+
+int convert( int n )
+{
+    // 引数が正数の場合
+    if ( n > 0 )
+        // そのまま関数solveに渡してreturn
+        return solve( n ) ;
+    else // 引数が負数の場合
+        // 絶対値を関数solveに渡して負数にしてreturn
+        return - solve( -n ) ;
+}
+
+
+// 入力
+int input()
+{
+    std::cout << "> " ;
+    int x{} ;
+    std::cin >> x ;
+    return x ;
+}
+
+// 出力
+void output( int binary )
+{
+    std::cout << binary << "\n"s ;
+}
+
+int main()
+{
+    // 入力、変換、出力のループ
+    while( true )
+    {
+        auto decimal = input() ;
+        auto binary = convert( decimal ) ;
+        output( binary ) ;
+    } 
 }
